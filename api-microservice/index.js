9c8a6ad5c51a3a8263f2b08d269bf6307e910bd9
Denis Beresnev <shanthi.bunch@gmail.com>
2024-06-07 12:20:41 +0300

    feat: cached api response

--- api-microservice/index.js
+++ api-microservice/index.js
@@ -12,60 +12,106 @@ const API_ENDPOINTS = {
 	chart: "https://api.coingecko.com/api/v3/coins/ethereum/market_chart?vs_currency=usd&days=30&interval=daily",
 };
 
+let infoCache = {
+	data: null,
+	timestamp: null,
+};
 app.get("/info", async (_, res) => {
 	try {
-		const dataResponses = await Promise.all(
-			Object.entries(API_ENDPOINTS).map(([key, url]) =>
-				axios
-					.get(url)
-					.then((response) => ({ key, data: response.data })),
-			),
-		);
+		const currentTime = new Date().getTime();
+		const cacheDuration = 1 * 60 * 1000;
 
-		const result = dataResponses.reduce((acc, { key, data }) => {
-			if (key === "chart") {
-				acc["prices"] = data?.prices.map((el) => el[1]);
-				acc["marketCaps"] = data?.market_caps.map((el) => el[1]);
-				acc["totalVolumes"] = data?.total_volumes.map((el) => el[1]);
-				acc["dates"] = data?.prices.map((el) => {
-					const date = new Date(el[0]);
+		if (
+			infoCache.data &&
+			currentTime - infoCache.timestamp < cacheDuration
+		) {
+			console.log("[INFO] from cache");
+			return res.json(infoCache.data);
+		} else {
+			const dataResponses = await Promise.all(
+				Object.entries(API_ENDPOINTS).map(([key, url]) =>
+					axios
+						.get(url)
+						.then((response) => ({ key, data: response.data })),
+				),
+			);
 
-					const year = date.getFullYear();
-					const month = date.getMonth() + 1;
-					const day = date.getDate();
+			const result = dataResponses.reduce((acc, { key, data }) => {
+				if (key === "chart") {
+					acc["prices"] = data?.prices.map((el) => el[1]);
+					acc["marketCaps"] = data?.market_caps.map((el) => el[1]);
+					acc["totalVolumes"] = data?.total_volumes.map(
+						(el) => el[1],
+					);
+					acc["dates"] = data?.prices.map((el) => {
+						const date = new Date(el[0]);
+						const year = date.getFullYear();
+						const month = date.getMonth() + 1;
+						const day = date.getDate();
+						return `${month}/${day}/${year}`;
+					});
+				} else if (key === "capitalization") {
+					acc[key] = data?.data?.market_cap_percentage;
+				} else {
+					acc[key] = {
+						currentPrice: data.at(0)?.current_price,
+						marketCap: data?.at(0)?.market_cap,
+						totalVolume: data?.at(0)?.total_volume,
+						dailyMax: data?.at(0)?.high_24h,
+						dailyMin: data?.at(0)?.low_24h,
+					};
+				}
+				return acc;
+			}, {});
 
-					return `${month}/${day}/${year}`;
-				});
-			} else if (key === "capitalization") {
-				acc[key] = data?.data?.market_cap_percentage;
-			} else {
-				acc[key] = {
-					currentPrice: data.at(0)?.current_price,
-					marketCap: data?.at(0)?.market_cap,
-					totalVolume: data?.at(0)?.total_volume,
-					dailyMax: data?.at(0)?.high_24h,
-					dailyMin: data?.at(0)?.low_24h,
-				};
-			}
-			return acc;
-		}, {});
+			infoCache.data = result;
+			infoCache.timestamp = currentTime;
 
-		res.json(result);
+			console.log("[INFO] from api");
+			res.json(result);
+		}
 	} catch (error) {
-		console.error("Error fetching info:", error?.response?.statusText);
-		res.status(500).send(error?.response?.statusText);
+		console.error(
+			"Error fetching info:",
+			error?.response?.statusText || error.message,
+		);
+		res.status(500).send(
+			error?.response?.statusText || "Error fetching info",
+		);
 	}
 });
 
+let newsCache = {
+	data: null,
+	timestamp: null,
+};
 app.get("/news", async (_, res) => {
 	try {
-		const response = await axios.get(
-			"https://api.coingecko.com/api/v3/news",
-		);
+		const currentTime = new Date().getTime();
+		const cacheDuration = 3 * 60 * 1000;
 
-		res.json(response.data);
+		if (
+			newsCache.data &&
+			currentTime - newsCache.timestamp < cacheDuration
+		) {
+			console.log("[NEWS] from cache");
+			return res.json(newsCache.data);
+		} else {
+			console.log("[NEWS] from api");
+			const response = await axios.get(
+				"https://api.coingecko.com/api/v3/news",
+			);
+
+			newsCache.data = response.data;
+			newsCache.timestamp = currentTime;
+
+			return res.json(response.data);
+		}
 	} catch (error) {
-		console.error("Error fetching info:", error);
+		console.error(
+			"Error fetching info:",
+			error?.response?.statusText || error.message,
+		);
 		res.status(500).send("Error fetching info");
 	}
 });
