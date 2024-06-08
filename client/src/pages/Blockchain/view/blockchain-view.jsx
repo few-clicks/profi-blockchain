40306b6b2d047d3d8e41c4c3d81f6ef7dba65746
Denis Beresnev <shanthi.bunch@gmail.com>
2024-06-08 22:57:00 +0300

    feat: connection to blockchain

--- client/src/pages/Blockchain/view/blockchain-view.jsx
+++ client/src/pages/Blockchain/view/blockchain-view.jsx
@@ -4,7 +4,11 @@ import Container from '@mui/material/Container';
 import Grid from '@mui/material/Unstable_Grid2';
 import Typography from '@mui/material/Typography';
 
+import Web3 from 'web3';
+
 import { posts } from 'src/_mock/blog';
+import employmentContractJSON from 'src/contracts/EmploymentContract.json';
+import employmentContractFactoryJSON from 'src/contracts/EmploymentContractFactory.json';
 
 import Iconify from 'src/components/iconify';
 
@@ -13,6 +17,23 @@ import PostSort from '../components/post-sort';
 import PostSearch from '../components/post-search';
 
 // ----------------------------------------------------------------------
+
+const CONTRACT_ADDRESS = employmentContractJSON.networks;
+const CONTRACT_ABI = employmentContractJSON.abi;
+
+const CONTRACT_FACTORY_ADDRESS = employmentContractFactoryJSON.networks['5777'].address;
+const CONTRACT_FACTORY_ABI = employmentContractFactoryJSON.abi;
+
+console.log('contract address', CONTRACT_ADDRESS);
+console.log('contract abi', CONTRACT_ABI);
+
+console.log('contract factory address', CONTRACT_FACTORY_ADDRESS);
+console.log('contract factory abi', CONTRACT_FACTORY_ABI);
+
+console.log('privider', Web3.givenProvider);
+const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
+const contract = new web3.eth.Contract(CONTRACT_FACTORY_ABI, CONTRACT_FACTORY_ADDRESS);
+console.log('contract', contract);
 
 export default function BlockchainView() {
   return (
